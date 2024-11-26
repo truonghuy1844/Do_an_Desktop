b04c89a847311280c4e33993e131c64513bcf22f
manhnguyenmont24 <manhmont24@gmail.com>
2024-11-26 11:23:44 +0700

    Designform

---
+++ PC_GUI/Quanlydonhang.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PC_GUI
+{
+    public partial class Quanlydonhang : Form
+    {
+        public Quanlydonhang()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Quanlydonhang_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
