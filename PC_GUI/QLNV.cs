9eadbbcdf65a355e8f1a4c6cac623cc45f184b46
31221022608@ueh.edu.vn <31221022608@ueh.edu.vn>
2024-11-27 13:48:56 +0700

    add QLNV form

--- PC_GUI/QLNV.cs
+++ PC_GUI/QLNV.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PC_GUI
 {
-    public partial class Form1 : Form
+    public partial class QLNV : Form
     {
-        public Form1()
+        public QLNV()
         {
             InitializeComponent();
         }
