f87fa9c169568acd5defd9e6f3eba652d1272bcd
31221022608@ueh.edu.vn <31221022608@ueh.edu.vn>
2024-12-13 13:07:11 +0700

    done QLKHO

--- PC_GUI/AddKho.cs
+++ PC_GUI/AddKho.cs
@@ -43,7 +43,7 @@ namespace PC_GUI
         private void txtMaKho_KeyPress(object sender, KeyPressEventArgs e)
         {
             TextBox textBox = sender as TextBox;
-            if (textBox != null && textBox.Text.Length >= 6 && !char.IsControl(e.KeyChar))
+            if (textBox != null && textBox.Text.Length >= 10 && !char.IsControl(e.KeyChar))
             {
                 e.Handled = true;
             }
